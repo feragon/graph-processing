9add42bea46b0ace59d5f368b19d51168c2369e5
Florian Roméo <florian.romeo@feragon.net>
2018-03-12 18:11:17 +0100

    Recherche

--- src/algorithm/dfs.cpp
+++ src/algorithm/dfs.cpp
@@ -1,9 +1,10 @@
 #include "dfs.h"
 
-DFS::DFS(const Graphe<EdgeData, VertexData>& graph) {
+DFS::DFS(const Graphe<EdgeData, VertexData>* graph, const Sommet<VertexData>* start) :
+        Search(graph, start) {
 
 }
 
-void DFS::search() {
+void DFS::analyzeVertex(const Sommet<VertexData>* vertex) {
 
 }
---
+++ src/algorithm/search.cpp
@@ -0,0 +1,11 @@
+#include <graph/graphe.h>
+#include "search.h"
+
+Search::Search(const Graphe <EdgeData, VertexData>* graph, const Sommet<VertexData>* start) {
+    _graph = graph;
+    _start = start;
+}
+
+void Search::search() {
+
+}
